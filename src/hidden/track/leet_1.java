7dbbe9ff79bbd966c1e8d39a9820c94216b8bbcd
Jason <qiulinhui@neotel.com.cn>
2020-09-24 15:54:10 +0800

    init

---
+++ src/hidden/track/leet_1.java
@@ -0,0 +1,14 @@
+package hidden.track;
+
+/**
+ * Author: Jason
+ * Date: 2020/9/24
+ * Time: 15:49
+ */
+public class leet_1 {
+
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
