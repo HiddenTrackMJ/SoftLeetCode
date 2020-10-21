5b39bfcd55d5fd5d55bdd75b984fe45bc4300217
HiddenTrack <1978354221@qq.com>
2020-10-21 23:09:24 +0800

    update

--- src/jian_main.cpp
+++ src/jian_main.cpp
@@ -6,13 +6,30 @@
 using namespace std;
 
 int main() {
-  cout << "Hello, this is SoftLeetCode." << endl;
+  //cout << "Hello, this is SoftLeetCode." << endl;
 
-  Solution s;
+  //Solution s;
 
-  /*  No.剑指 Offer 03. 数组中重复的数字 */
-  vector<int> nums = {2, 3, 1, 0, 2, 5, 3};
-  auto res_03 = s.findRepeatNumber(nums);
-  std::cout << "res_03: " << res_03 << std::endl;
+  ///*  No.剑指 Offer 03. 数组中重复的数字 */
+  //vector<int> nums = {2, 3, 1, 0, 2, 5, 3};
+  //auto res_03 = s.findRepeatNumber(nums);
+  //std::cout << "res_03: " << res_03 << std::endl;
 
+  int n, x = 0, y = 0;
+  std::cin >> n;
+  char a[100];
+  int b[100];
+  for (int i = 0; i < n; i++) {
+    std::cin >> a[i];
+    //std::cin >> " ";
+    std::cin >> b[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (a[i] == 'W') x = x - 2;
+    if (a[i] == 'E') x = x + 2;
+    if (a[i] == 'S') y = y - 2;
+    if (a[i] == 'N') y = y + 2;
+  }
+  cout << x << " " << y << endl;
 }
